2fb8a642dfb27f84c9929f4a75edb366098bc1a5
aapanasov <aapanasov@gmail.com>
2022-11-26 15:18:00 +0300

    feat: fs:create done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,12 @@
+import { writeFile } from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const filePath = new URL('./files/fresh.txt', import.meta.url);
+        await writeFile(filePath, 'I am fresh and young', { flag: 'wx' });
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
