66e23bfdb441cd102d00e7c8040a0db1dca738f9
aapanasov <aapanasov@gmail.com>
2022-11-26 14:56:30 +0300

    feat: fs:read done

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import { readFile } from 'node:fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const filePath = new URL('./files/fileToRead.txt', import.meta.url);
+        const content = await readFile(filePath, { encoding: 'utf8' });
+        console.log(content);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
