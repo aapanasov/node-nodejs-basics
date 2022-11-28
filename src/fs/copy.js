70e835f7fd6cbdec7d5611dd2aae360b93d2968c
aapanasov <aapanasov@gmail.com>
2022-11-28 15:19:34 +0300

    feat: fs:copy done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import { cp, mkdir } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+
+const src = join(currentDir + '/files');
+const dest = join(currentDir + '/files_copy');
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        await mkdir(dest, { recursive: false });
+        await cp(src, dest, { recursive: true, errorOnExist: true, force: false });
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 copy();
