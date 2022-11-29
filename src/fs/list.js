e58fa34b0cffddd0cab515ab7f162279ec7ed6be
aapanasov <aapanasov@gmail.com>
2022-11-29 12:27:18 +0300

    feat: fs:list done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import { fileURLToPath } from 'node:url';
+import { readdir } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+const dirToRead = join(currentDir + '/files');
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const files = await readdir(dirToRead);
+        files.forEach((file) => console.log(file));
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
