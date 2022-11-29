9093275201d1e49e870149f8746b1742cedcba59
aapanasov <aapanasov@gmail.com>
2022-11-29 15:55:22 +0300

    feat: cli:args done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const inputArgs = process.argv.slice(2);
+
+    const result = inputArgs
+        .map((arg, index, array) => {
+            if (array[index + 1] && arg.startsWith('--')) {
+                return `${arg.slice(2)} is ${array[index + 1]}`;
+            }
+        })
+        .filter((arg) => arg)
+        .join(', ');
+
+    console.log(result);
 };
 
 parseArgs();
