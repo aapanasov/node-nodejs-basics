f3a79489ad58acb7baf61294fbec30bd4c7041b2
aapanasov <aapanasov@gmail.com>
2022-11-30 16:05:49 +0300

    feat: hash done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { readFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { createHash } from 'node:crypto';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = join(__dirname, './files/fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileBuffer = await readFile(filePath);
+    const hashSum = createHash('sha256').update(fileBuffer).digest('hex');
+
+    console.log(hashSum);
 };
 
 await calculateHash();
