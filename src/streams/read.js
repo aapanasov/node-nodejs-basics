154b0fa1e8a68cd79e0f6b8a3cc34d09116a6aa8
aapanasov <aapanasov@gmail.com>
2022-12-02 17:20:21 +0300

    feat: streams:read done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fileToRead = join(__dirname, './files/fileToRead.txt');
+const streamToRead = createReadStream(fileToRead, { encoding: 'utf8' });
+
 const read = async () => {
-    // Write your code here 
+    streamToRead.on('data', (chunk) => process.stdout.write(chunk));
+    streamToRead.on('error', (err) => { throw new Error(err); });
 };
 
 await read();
