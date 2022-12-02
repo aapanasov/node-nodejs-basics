055bd8e35584db5b6ea39f5cd743b3e6c52815e5
aapanasov <aapanasov@gmail.com>
2022-12-02 18:11:14 +0300

    feat: streams:transform done

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,24 @@
+import { Transform, pipeline } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+
+    const transformStream = new Transform({
+        transform(chunk, enc, callback) {
+            const chunkString = chunk.toString().trim();
+            const reversedChunk = chunkString.split('').reverse().join('');
+
+            this.push(reversedChunk + '\n');
+
+            callback();
+        }
+    });
+
+    pipeline(
+        process.stdin,
+        transformStream,
+        process.stdout,
+        (err) => console.log(err)
+    );
 };
 
 await transform();
