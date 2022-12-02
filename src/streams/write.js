b7d17800ca81b80e8e7e5a8cf6d556b3e101c2f7
aapanasov <aapanasov@gmail.com>
2022-12-02 17:36:12 +0300

    feat: streams:write done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fileToWrite = join(__dirname, './files/fileToWrite.txt');
+const streamToWrite = createWriteStream(fileToWrite, { encoding: 'utf8' });
+
 const write = async () => {
-    // Write your code here 
+    process.stdin.on('data', (chunk) => streamToWrite.write(chunk));
 };
 
 await write();
