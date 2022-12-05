2a2b5579aa619d81182c43a15522b7e9d5e62227
aapanasov <aapanasov@gmail.com>
2022-12-05 16:40:27 +0300

    feat: cp done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,17 @@
+import { spawn } from 'node:child_process';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fileToSpawn = join(__dirname, './files/script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+
+    const childProcess = spawn('node', [fileToSpawn, ...args]);
+    childProcess.stdout.pipe(process.stdout);
+    process.stdin.pipe(childProcess.stdin);
+
 };
 
-spawnChildProcess();
+// Put your arguments in function call to test this functionality
+spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
